d10bad1b39f38facb075e29420b278c340a071c0
RunningAgama <carmov106@proton.me>
2024-08-09 11:29:11 -0300

    preparo pra futuras atualizações

---
+++ EB/src/pages/Register/index.jsx
@@ -0,0 +1,13 @@
+import {Form, Alert} from 'react-bootstrap'
+
+const register = ()=>{
+    return(
+        <div className="register">
+            <div className="card">
+                <form onSubmit={}>
+                    <input type="text" onChange={null}  placeholder='email'/>
+                </form>
+            </div>
+        </div>
+    )
+}
---
+++ server/routes/userRoute.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import { listUsers, loginUser, userRegister } from '../controllers/userControllers.js'
+
+const app = express()
+const router = express.Router()
+
+router.post('/register', userRegister)
+router.get('/listusers/', listUsers)
+router.post('/login', loginUser)
+
+export default router
